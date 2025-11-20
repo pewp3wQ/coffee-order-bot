1aa5520cd2828d9d212e37dd1697e3c36f92ea4d
pewp3wQ <igoreshkeufs@gmail.com>
2025-11-20 23:13:48 +0900

    some change for get db_pool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 
+import psycopg_pool
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiogram_dialog import setup_dialogs
 from aiohttp import web
@@ -12,6 +13,7 @@ from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
 from redis.asyncio import Redis
 
 from config.config import load_config, Config
+from database.connection import get_pg_pool
 from dialogs import main_menu, order
 from middleware.db_connection import DataBaseMiddleware
 from handlers import group
@@ -19,6 +21,7 @@ from menu.set_menu import set_user_menu, set_description
 
 config: Config = load_config()
 logger = logging.getLogger(__name__)
+app = web.Application()
 
 
 async def on_startup(bot: Bot) -> None:
@@ -31,6 +34,17 @@ async def on_startup(bot: Bot) -> None:
     logger.info('Вебхук установлен')
     # await bot.set_webhook(f"{config.webhook.base_url}{config.webhook.path}", secret_token=config.webhook.secret)
 
+
+
+async def create_pool(app: web.Application):
+    db_pool = await get_pg_pool(
+            db_name=config.db.name,
+            host=config.db.host,
+            port=config.db.port,
+            user=config.db.user,
+            password=config.db.password,
+        )
+    return db_pool
 
 async def main() -> None:
     storage = RedisStorage(
@@ -49,7 +63,16 @@ async def main() -> None:
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
     dp = Dispatcher(storage=storage)
-    await bot.delete_webhook()
+
+    db_pool: psycopg_pool.AsyncConnectionPool = await get_pg_pool(
+        db_name=config.db.name,
+        host=config.db.host,
+        port=config.db.port,
+        user=config.db.user,
+        password=config.db.password,
+    )
+
+    # await bot.delete_webhook()
     dp.startup.register(on_startup)
 
     logger.info('Router including')
@@ -63,18 +86,17 @@ async def main() -> None:
     logger.info("Including middlewares...")
     dp.update.middleware(DataBaseMiddleware())
 
-    await dp.start_polling(bot)
+    # await dp.start_polling(bot)
 
-    # app = web.Application()
-    # webhook_requests_handler = SimpleRequestHandler(
-    #     dispatcher=dp,
-    #     bot=bot,
-    #     secret_token=config.webhook.secret,
-    # )
-    # webhook_requests_handler.register(app, path=config.webhook.path)
-    #
-    # setup_application(app, dp, bot=bot)
-    # web.run_app(app, host=config.webhook.server, port=config.webhook.port)
+    # app.on_startup.append(create_pool)
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=config.webhook.secret,
+        dp_pool=db_pool
+    )
+    webhook_requests_handler.register(app, path=config.webhook.path)
+    setup_application(app, dp, bot=bot)
 
 
 if __name__ == '__main__':
@@ -82,4 +104,4 @@ if __name__ == '__main__':
         level=logging.getLevelName(level=config.log.level),
         format=config.log.format
     )
-    asyncio.run(main())
+    web.run_app(app, host=config.webhook.server, port=int(config.webhook.port))
