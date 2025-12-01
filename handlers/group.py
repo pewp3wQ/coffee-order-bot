64cc1ef9ef61ca77be01b39d8a02806b09f7cf48
pewp3wQ <igoreshkeufs@gmail.com>
2025-12-01 22:23:20 +0900

    convert type to int group_id

--- handlers/group.py
+++ handlers/group.py
@@ -44,7 +44,7 @@ async def took_order(callback: CallbackQuery, bot: Bot, conn: AsyncConnection):
     await callback.message.edit_text(text=callback.message.text, reply_markup=keyboard)
 
 
-@router.callback_query(F.data == 'finish', F.message.chat.id == config.group.group_id)
+@router.callback_query(F.data == 'finish', F.message.chat.id == int(config.group.group_id))
 async def took_order(callback: CallbackQuery):
         await callback.answer()
 
