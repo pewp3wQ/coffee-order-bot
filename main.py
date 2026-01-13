7ce4e29e00d4c8cd7d7f8669940a7df3cf8f094a
pewp3wQ <igoreshkeufs@gmail.com>
2026-01-13 21:04:54 +0900

    webhook turn on

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ async def on_startup(bot: Bot) -> None:
     await set_user_menu(bot)
     await set_description(bot)
 
-    # logger.info('Вебхук установлен')
-    # await bot.set_webhook(url=f"{config.webhook.base_url}{config.webhook.path}",
-    #                       secret_token=config.webhook.secret,
-    #                       drop_pending_updates=True)
+    logger.info('Вебхук установлен')
+    await bot.set_webhook(url=f"{config.webhook.base_url}{config.webhook.path}",
+                          secret_token=config.webhook.secret,
+                          drop_pending_updates=True)
 
 
 async def on_shutdown(dispatcher: Dispatcher, bot: Bot):
@@ -42,7 +42,7 @@ async def on_shutdown(dispatcher: Dispatcher, bot: Bot):
 
 
 async def main():
-    # log_config.setup_logging()
+    log_config.setup_logging()
 
     storage = RedisStorage(
         redis=Redis(
@@ -86,22 +86,22 @@ async def main():
     logger.info("Including middlewares...")
     dp.update.middleware(DataBaseMiddleware())
 
-    await dp.start_polling(bot)
-    # app = web.Application()
-    # webhook_requests_handler = SimpleRequestHandler(
-    #     dispatcher=dp,
-    #     bot=bot,
-    #     secret_token=config.webhook.secret
-    # )
-    # webhook_requests_handler.register(app, path=config.webhook.path)
-    # setup_application(app, dp, bot=bot)
-    # return app
+    # await dp.start_polling(bot)
+    app = web.Application()
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=config.webhook.secret
+    )
+    webhook_requests_handler.register(app, path=config.webhook.path)
+    setup_application(app, dp, bot=bot)
+    return app
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.getLevelName(level=config.log.level),
-        format=config.log.format
-    )
-    asyncio.run(main())
-    # web.run_app(main(), host=config.webhook.server, port=int(config.webhook.port))
+    # logging.basicConfig(
+    #     level=logging.getLevelName(level=config.log.level),
+    #     format=config.log.format
+    # )
+    # asyncio.run(main())
+    web.run_app(main(), host=config.webhook.server, port=int(config.webhook.port))
